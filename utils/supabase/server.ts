79b0f386a84ea6e4d7d0bc8cf4a03e11ca1143da
simmmpleweb <hello@simmmple.com>
2024-09-07 00:38:35 +0300

    Test

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -8,8 +8,10 @@ export const createClient = () => {
   const cookieStore = cookies();
   return createServerClient<Database>(
     // Pass Supabase URL and anonymous key from the environment to the client
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    // process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdW1yd2VxaXN2anZxeGRvbHR1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNTYyMTYyNSwiZXhwIjoyMDMxMTk3NjI1fQ.hDpNR4AnTe9WP78T1whEkyh9vy8Yah5ahYKZhZKB4ro",
+    // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdW1yd2VxaXN2anZxeGRvbHR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MjE2MjUsImV4cCI6MjAzMTE5NzYyNX0.FeMFgqJyZ0ZJYi4oqHRFmSZSdURwtMI1mbIToN5fx20",
 
     // Define a cookies object with methods for interacting with the cookie store and pass it to the client
     {
