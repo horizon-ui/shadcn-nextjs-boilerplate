f8bbb956fd0e21e30d74007cff3abc9cce549a05
simmmpleweb <hello@simmmple.com>
2024-09-09 20:23:51 +0300

    Ref cookies added

--- app/dashboard/ai-chat/page.tsx
+++ app/dashboard/ai-chat/page.tsx
@@ -11,6 +11,28 @@ export default async function AiChat() {
     getUserDetails(supabase)
   ]);
 
+  // Function to get URL parameter by name
+  function getParameterByName(name: string) {
+    const url = new URL(window.location.href);
+    console.log(url.searchParams.get(name));
+    return url.searchParams.get(name);
+  }
+  // Function to set a cookie
+  function setCookie(name: string, value: any, days: any) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = 'expires=' + date.toUTCString();
+    console.log(name, value, days);
+    document.cookie = name + '=' + value + ';' + expires + ';path=/';
+  }
+
+  // Capture the 'ref' parameter from the URL
+  const ref = getParameterByName('ref');
+
+  // If the 'ref' parameter exists, store it in a cookie
+  if (ref) {
+    setCookie('creativetimref', ref, 1); // Store cookie for 1 day
+  }
   if (!user) {
     return redirect('/shadcn-nextjs-boilerplate/dashboard/signin');
   }
