977e77e893086b091588b8bd3ceef0f3b40af927
simmmpleweb <hello@simmmple.com>
2024-09-10 10:46:12 +0300

    Middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -2,30 +2,6 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from '@/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-
-  // Function to get URL parameter by name
-  function getParameterByName(name: string) {
-    const url = new URL(window.location.href);
-    console.log(url.searchParams.get(name));
-    return url.searchParams.get(name);
-  }
-  // Function to set a cookie
-  function setCookie(name: string, value: any, days: any) {
-    const date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    const expires = 'expires=' + date.toUTCString();
-    console.log(name, value, days);
-    document.cookie = name + '=' + value + ';' + expires + ';path=/';
-  }
-
-  // Capture the 'ref' parameter from the URL
-  const ref = getParameterByName('ref');
-
-  // If the 'ref' parameter exists, store it in a cookie
-  if (ref) {
-    setCookie('creativetimref', ref, 1); // Store cookie for 1 day
-  }
-  
   return await updateSession(request);
   
 }
