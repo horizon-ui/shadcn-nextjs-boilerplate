3ceac79ccb52b550ae44bf5dd8b92db8dbb98cf9
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-02-04 16:34:13 +0700

    Tranform in to storybook that based on ts

---
+++ frontend/home/.storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],  
+  // Any other configurations from your main.js should go here.
+};
+
+export default config;
