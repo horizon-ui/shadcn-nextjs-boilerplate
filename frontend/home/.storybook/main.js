7198cf0a122fced59a85645fa509d6abe1570bee
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-02-04 13:56:21 +0700

    Install Storybook

---
+++ frontend/home/.storybook/main.js
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions'
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  staticDirs: ['../public']
+};
+export default config;
